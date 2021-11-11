a37c808a8e17a417b904285d60c9754f3a86e062
kerols1234 <kerolssamer348@gmail.com>
2021-11-11 14:16:15 +0200

    Initial commit

---
+++ app/src/main/java/com/example/pushnotification/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.pushnotification;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "PushNotification";
+    private static final String CHANNEL_ID = "101";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        createNotificationChannel();
+      //  getToken();
+       getTopic();
+    }
+
+    private void getToken() {
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
+            @Override
+            public void onComplete(@NonNull Task<String> task) {
+                if(!task.isSuccessful()){
+                    Log.d(TAG, "onComplete: Failed to get token");
+                }
+
+                String token = task.getResult();
+                Log.d(TAG, "onComplete: " + token);
+            }
+        });
+    }
+
+    private void getTopic(){
+        FirebaseMessaging.getInstance().subscribeToTopic("weather")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String msg = "good";
+                        if (!task.isSuccessful()) {
+                            msg = "failed";
+                        }
+                        Log.d(TAG, msg);
+                    }
+                });
+    }
+
+    private void createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "firebaseNotificationChannel";
+            String description = "Receive firebase notification";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+}
