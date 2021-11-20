ee4eeaa5219ca2616b9bc47f31e79c03d2ad9475
nadahkhaledd <nadahkhaledd@gmail.com>
2021-11-20 20:07:56 +0200

    added firebase storage

--- app/src/main/java/com/example/pushnotification/MainActivity.java
+++ app/src/main/java/com/example/pushnotification/MainActivity.java
@@ -2,25 +2,35 @@ package com.example.pushnotification;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 import com.example.pushnotification.Notification.Helper;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "PushNotification";
     private static final String CHANNEL_ID = "101";
+    private static final int PICK_FILE_REQUEST_CODE = 300;
 
     String token;
 
@@ -32,7 +42,62 @@ public class MainActivity extends AppCompatActivity {
         createNotificationChannel();
         getToken();
         getTopic();
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+
+        View upload = findViewById(R.id.upload);
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectFile();
+            }
+        });
     }
+
+    private void selectFile()
+    {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        startActivityForResult(intent,PICK_FILE_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_FILE_REQUEST_CODE && resultCode == RESULT_OK)
+        {
+            Uri path = data.getData();
+            uploadFile(path);
+
+        }
+    }
+
+    private void uploadFile(Uri path)
+    {
+        StorageReference root =  FirebaseStorage.getInstance().getReference();
+        final StorageReference file = root.child("images/" + path.getLastPathSegment());
+        UploadTask taskUpload = file.putFile(path);
+        taskUpload.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                float percentage = (snapshot.getBytesTransferred() / (float)snapshot.getTotalByteCount()) * 100.0f;
+
+            }
+        });
+        taskUpload.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                file.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Uri> task) {
+                        Log.e("Download link: ", task.getResult().toString());
+
+                    }
+                });
+            }
+        });
+
+    }
+
 
     private void getToken() {
         FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
